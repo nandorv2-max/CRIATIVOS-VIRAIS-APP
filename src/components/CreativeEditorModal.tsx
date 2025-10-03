e7488b30fc18339087d86bb8db63aefbc721ec30
[dyad] <nandorv2@gmail.com>
2025-10-03 19:28:50 -0300

    Reverted all changes back to version 884884f501e02bb3a284cf8bf9090bb7e782926f

--- src/components/CreativeEditorModal.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CreativeEditorModal: React.FC = () => {
-    // Este componente parece ser um placeholder ou estar em desenvolvimento.
-    // Vou retornar null para evitar erros, mas ele pode ser implementado no futuro.
-    return null;
-};
-
-export default CreativeEditorModal;
